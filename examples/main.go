53e1973a35bd80f0b4a12d59c440797eab2fc9ab
OwlAwake <paayasakotkar@gmail.com>
2026-05-25 14:03:20 +0530

    rum-v2

--- examples/main.go
+++ examples/main.go
@@ -1,2 +1,4 @@
 // Package example implements the whole example of the pacakges
+// disclaimer: the examples are written by gen ai
+// do test it and incase if any error comment down the issues
 package example
